513a42817bd6dbd583851b836ea73300584dfac9
Tum Chaturapruek <Tum_Chaturapruek@HMC.Edu>
2012-10-23 03:25:35 -0700

    Revert "Fix the directory"
    This reverts commit 9035872d15c54a5823baa906380f99bd89271056.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '1f4f4e1b9b555ee5a4b76c3a9233b737ca3ccb5d957ddd54556b9fa2670b3fdb9944cec2dbdbbaf75a74e01b952e47384016d85e6e72572b6f525b3abb18949c'
